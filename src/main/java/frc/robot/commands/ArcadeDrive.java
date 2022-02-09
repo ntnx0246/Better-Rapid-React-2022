0f5a3d643dfdb7db9edbaca5ce7527566cedbaa0
potttaaatttooo <nathannguyen202@gmail.com>
2022-02-08 17:19:39 -0800

    added shuffleboard

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -34,8 +34,10 @@ public class ArcadeDrive extends CommandBase {
   public void execute() {
     if (driveTrain.getSlowMode()) {
       driveTrain.arcadeDrive((drivePad.getRightAnalogXAxis()*Constants.SLOW_MODE), (drivePad.getLeftAnalogYAxis()*Constants.SLOW_MODE));
+      SmartDashboard.putBoolean("Slow Mode: ", true);
     } else {
       driveTrain.arcadeDrive((drivePad.getRightAnalogXAxis()*Constants.REGULAR_MODE), (drivePad.getLeftAnalogYAxis()*Constants.REGULAR_MODE));
+      SmartDashboard.putBoolean("Slow Mode: ", false);
     }
     
   }
