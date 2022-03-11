4a1bc30dda62a7a2c16c4f771ffa1dbe4062843b
potttaaatttooo <nathannguyen202@gmail.com>
2022-03-11 08:53:53 -0800

    ramprate

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -72,6 +72,8 @@ public final class Constants {
   public static final int CRUISE_VELOCITY = 7000; // max is around 21500
   public static final int ACCELERATION = 3500;
   public static final int ERROR_THRESHOLD = 500;
+  public static final double OPEN_LOOP_RAMP = 5;
+  public static final double CLOSED_LOOP_RAMP = 5;
 
   public static final double ERROR_ANGLE_TOLERANCE = 5;
   public static final double AUTO_CLIMB_TOLERANCE = 50;
