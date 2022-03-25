024082e88187c6a04cc3fd7465d4ba2321d50ea0
Gabriel Han <gabrielhan104@gmail.com>
2022-03-25 00:49:49 -0700

    hi

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,7 @@ import frc.robot.utils.ShuffleBoard;
 
 public class RobotContainer {
   private final LogitechGamingPad drivePad = new LogitechGamingPad(0);
+  private final LogitechGamingPad opPad = new LogitechGamingPad(1);
 
   private final DriveTrain driveTrain = new DriveTrain();
   private final Shooter shooter = new Shooter();
@@ -46,6 +47,17 @@ public class RobotContainer {
   // 7);
   // private final JoystickButton driveStartButton = new JoystickButton(drivePad,
   // 8);
+
+  private final JoystickButton opA = new JoystickButton(opPad, 1);
+  private final JoystickButton opB = new JoystickButton(opPad, 2);
+  private final JoystickButton opX = new JoystickButton(opPad, 3);
+  private final JoystickButton opY = new JoystickButton(opPad, 4);
+  private final JoystickButton opLeftBumper = new JoystickButton(opPad, 5);
+  private final JoystickButton opRightBumper = new JoystickButton(opPad, 6);
+  private final JoystickButton opBackButton = new JoystickButton(opPad, 7);
+  private final JoystickButton opStartButton = new JoystickButton(opPad, 8);
+
+  
 
   private final ShuffleBoard shuffleBoard = new ShuffleBoard(
       intake, shooter, driveTrain, navX);
