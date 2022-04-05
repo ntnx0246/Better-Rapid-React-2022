67040c64b44442832d4e641555765c04fce95215
Gabriel Han <gabrielhan104@gmail.com>
2022-04-04 21:26:20 -0700

    hi

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -17,8 +17,7 @@ public class Vision extends SubsystemBase {
 
   public Vision() {
 
-    // CameraServer.startAutomaticCapture();
-
+    CameraServer.startAutomaticCapture();
 
     // visionCam = new UsbCamera("cam0", 1);
     // visionCam = CameraServer.startAutomaticCapture();
